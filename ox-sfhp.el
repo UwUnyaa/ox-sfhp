327808adccd4358649c06acff95e5eeeea6bfb49
DoMiNela10 <retrogamingdomi@wp.pl>
2016-08-29 17:34:02 +0200

    bumped version

--- ox-sfhp.el
+++ ox-sfhp.el
@@ -1,6 +1,6 @@
 ;;; ox-sfhp.el - export from org-mode to a single file HTML presentation
 ;;; -*- coding: utf-8 -*-
-;;; Version: 1.2.0
+;;; Version: 1.2.1
 
 ;; Author: DoMiNeLa10 (https://github.com/DoMiNeLa10)
 
